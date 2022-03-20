3415fbcc7bd4565af8c6a5ede3d4a21a37e828ca
joonashak <hakkijoo@gmail.com>
2022-03-20 21:35:53 +0200

    Set up mongoose and db connection

--- cms/src/app.module.ts
+++ cms/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from "@nestjs/common";
+import { MongooseModule } from "@nestjs/mongoose";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
+import { DATABASE_URL } from "./config";
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot(DATABASE_URL)],
   controllers: [AppController],
   providers: [AppService],
 })
