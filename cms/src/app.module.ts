cfb7d8ded6f3888c9827e06adbbf58252360fec4
joonashak <hakkijoo@gmail.com>
2022-03-20 21:54:35 +0200

    Install GraphQL

--- cms/src/app.module.ts
+++ cms/src/app.module.ts
@@ -1,12 +1,21 @@
+import { ApolloDriver, ApolloDriverConfig } from "@nestjs/apollo";
 import { Module } from "@nestjs/common";
+import { GraphQLModule } from "@nestjs/graphql";
 import { MongooseModule } from "@nestjs/mongoose";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { DATABASE_URL } from "./config";
 
 @Module({
-  imports: [MongooseModule.forRoot(DATABASE_URL)],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    MongooseModule.forRoot(DATABASE_URL),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: true,
+      playground: true,
+      debug: false,
+    }),
+  ],
+  providers: [AppService, AppController],
 })
 export class AppModule {}
