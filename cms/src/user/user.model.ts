eabe0e9775467440c5aa1a1cfebb29f9b804f796
joonashak <hakkijoo@gmail.com>
2022-03-21 19:58:48 +0200

    Add create user method

--- cms/src/user/user.model.ts
+++ cms/src/user/user.model.ts
@@ -11,7 +11,7 @@ export class User {
   id: string;
 
   @Field()
-  @Prop()
+  @Prop({ unique: true })
   username: string;
 
   @Field()
--- cms/src/user/user.service.ts
+++ cms/src/user/user.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from "@nestjs/common";
 import { InjectModel } from "@nestjs/mongoose";
+import { hash } from "bcrypt";
 import { Model } from "mongoose";
+import { CreateUserDto } from "./dto/createUser.dto";
 import { User, UserDocument } from "./user.model";
 
 @Injectable()
@@ -10,5 +12,12 @@ export class UserService {
   async findAll(): Promise<User[]> {
     const users = this.userModel.find();
     return users;
+  }
+
+  async create(input: CreateUserDto): Promise<User> {
+    const { password, ...rest } = input;
+    const passwordHash = await hash(password, 12);
+    const newUser = await this.userModel.create({ ...rest, passwordHash });
+    return newUser;
   }
 }
