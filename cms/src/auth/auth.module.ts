18e3ab44c5f6d8baf2c00a55f96280d1aee7fecb
joonashak <hakkijoo@gmail.com>
2022-03-21 23:41:17 +0200

    Add local auth strategy

--- cms/src/auth/auth.module.ts
+++ cms/src/auth/auth.module.ts
@@ -1,9 +1,21 @@
 import { Module } from "@nestjs/common";
-import { UserModule } from "src/user/user.module";
-import { UserService } from "src/user/user.service";
+import { JwtModule } from "@nestjs/jwt";
+import { PassportModule } from "@nestjs/passport";
+import { JWT_LIFETIME, JWT_SECRET } from "../config";
+import { UserModule } from "../user/user.module";
+import { UserService } from "../user/user.service";
+import { AuthService } from "./auth.service";
+import { LocalStrategy } from "./strategies/local.strategy";
 
 @Module({
-  imports: [UserModule],
-  providers: [UserService],
+  imports: [
+    UserModule,
+    PassportModule,
+    JwtModule.register({
+      secret: JWT_SECRET,
+      signOptions: { expiresIn: JWT_LIFETIME },
+    }),
+  ],
+  providers: [UserService, LocalStrategy, AuthService],
 })
 export class AuthModule {}
--- cms/src/config.ts
+++ cms/src/config.ts
@@ -3,3 +3,5 @@ export const NODE_ENV: string = process.env.NODE_ENV;
 export const DEV_TOOLS_ENABLED: boolean = ["development", "test"].includes(
   NODE_ENV,
 );
+export const JWT_SECRET: string = process.env.JWT_SECRET || "";
+export const JWT_LIFETIME: string = process.env.JWT_LIFETIME || "30d";
