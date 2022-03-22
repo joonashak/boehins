34462b7369f31a8b67fb80f3862efbb05588429f
joonashak <hakkijoo@gmail.com>
2022-03-22 18:12:50 +0200

    Implement login guard

--- cms/src/auth/auth.resolver.ts
+++ cms/src/auth/auth.resolver.ts
@@ -1,12 +1,14 @@
+import { UseGuards } from "@nestjs/common";
 import { Args, Mutation, Resolver } from "@nestjs/graphql";
 import { AuthService } from "./auth.service";
 import { LoginResponse } from "./dto/loginResponse.dto";
+import { LoginGuard } from "./guards/login.guard";
 
 @Resolver()
 export class AuthResolver {
   constructor(private authService: AuthService) {}
 
-  // TODO: Create LoginGuard to handle acual auth.
+  @UseGuards(LoginGuard)
   @Mutation((returns) => LoginResponse)
   async login(
     @Args("username") username: string,
