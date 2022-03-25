cad9cb1a8c75cba4fa0a103df14a56e87fa17593
joonashak <hakkijoo@gmail.com>
2022-03-25 20:29:48 +0200

    More tests

--- cms/src/user/user.service.spec.ts
+++ cms/src/user/user.service.spec.ts
@@ -1,5 +1,6 @@
 import { getModelToken } from "@nestjs/mongoose";
 import { Test } from "@nestjs/testing";
+import { async } from "rxjs";
 import { UserService } from "./user.service";
 
 describe("UserService", () => {
@@ -15,6 +16,11 @@ describe("UserService", () => {
             create: jest
               .fn()
               .mockImplementation(({ username }) => ({ username })),
+            // TODO: This mess should be replaced with a library.
+            findOne: jest.fn().mockImplementation(({ username }) => ({
+              then: (resolve) => resolve({ username }),
+              select: () => ({ username, passwordHash: "zxcv" }),
+            })),
           },
         },
       ],
@@ -24,8 +30,20 @@ describe("UserService", () => {
   });
 
   it("Create user", async () => {
-    expect(await service.create({ username: "asd", password: "123" })).toStrictEqual({
+    expect(
+      await service.create({ username: "asd", password: "123" }),
+    ).toStrictEqual({
       username: "asd",
     });
+  });
+
+  it("Find one user", async () => {
+    expect(await service.findOne("asd")).toStrictEqual({ username: "asd" });
+  });
+
+  it("Find one user with password hash", async () => {
+    expect(
+      await service.findOne("asd", { withPasswordHash: true }),
+    ).toStrictEqual({ username: "asd", passwordHash: "zxcv" });
   });
 });
