14cdd4e8d8f65d125efbfe0bcbdce67389ad4159
joonashak <hakkijoo@gmail.com>
2022-04-05 21:46:04 +0300

    Add unit tests for access denied cases

--- cms/src/auth/guards/tokenAuth.guard.spec.ts
+++ cms/src/auth/guards/tokenAuth.guard.spec.ts
@@ -6,6 +6,7 @@ import { createMock } from "@golevelup/ts-jest";
 import ms from "ms";
 import { SessionService } from "../session/session.service";
 import { TokenAuthGuard } from "./tokenAuth.guard";
+import { AuthenticationError } from "apollo-server-express";
 
 const createContextWithHeaders = (headers: any): ExecutionContext => {
   const args: any = [{}, {}, { req: { headers } }, {}];
@@ -66,5 +67,54 @@ describe("TokenAuthGuard", () => {
     await expect(guard.canActivate(context)).resolves.toEqual(true);
     expect(sessionService.findOneById).toBeCalledWith("jkl");
     expect(sessionService.findOneById).toBeCalledTimes(1);
+  });
+
+  it("Access denied when token is missing", async () => {
+    const context = createContextWithHeaders({});
+    await expect(guard.canActivate(context)).rejects.toThrowError(AuthenticationError);
+  });
+
+  it("Access denied when token is invalid", async () => {
+    const context = createContextWithHeaders({
+      accesstoken: "djoaishjdiuwybf9538b9g3ubr",
+    });
+    await expect(guard.canActivate(context)).rejects.toThrowError(AuthenticationError);
+  });
+
+  it("Access denied when token is valid but does not contain a session ID", async () => {
+    const accessToken = jwtService.sign({});
+    const context = createContextWithHeaders({ accesstoken: accessToken });
+    await expect(guard.canActivate(context)).rejects.toThrowError(AuthenticationError);
+  });
+
+  it("Access denied when session is not found", async () => {
+    const accessToken = jwtService.sign({ sessionId: "jkl" });
+    const context = createContextWithHeaders({ accesstoken: accessToken });
+    jest.spyOn(sessionService, "findOneById").mockResolvedValueOnce(null);
+    await expect(guard.canActivate(context)).rejects.toThrowError(AuthenticationError);
+  });
+
+  it("Access denied when session has expired", async () => {
+    const accessToken = jwtService.sign({ sessionId: "jkl" });
+    const context = createContextWithHeaders({ accesstoken: accessToken });
+    jest.spyOn(sessionService, "findOneById").mockResolvedValueOnce({
+      id: "jkl",
+      expiresAt: new Date(Date.now() - ms("1d")),
+      user: { username: "asd", id: "asd" },
+    });
+
+    await expect(guard.canActivate(context)).rejects.toThrowError(AuthenticationError);
+  });
+
+  it("Access denied when user is not found", async () => {
+    const accessToken = jwtService.sign({ sessionId: "jkl" });
+    const context = createContextWithHeaders({ accesstoken: accessToken });
+    jest.spyOn(sessionService, "findOneById").mockResolvedValueOnce({
+      id: "jkl",
+      expiresAt: new Date(Date.now() + ms("30d")),
+      user: null,
+    });
+
+    await expect(guard.canActivate(context)).rejects.toThrowError(AuthenticationError);
   });
 });
